c5042b18f6bcaef7f40d8c72cd6d5c694ab12f03
MohaiminulEraj <eraj471@gmail.com>
2024-07-02 17:46:31 +0600

    customer entity migration added

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
+use Exception;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Customers retrieved successfully',
+            'result' => CustomerResource::collection(Customer::paginate($request->limit ?? 10))
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $customer = new Customer();
+            $customer->name = $request->name;
+            $customer->email = $request->email;
+            $customer->phone = $request->phone;
+            $customer->address = $request->address;
+            $customer->currency = $request->currency;
+            $customer->vat = $request->vat;
+            $customer->save();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Customer created successfully',
+                'result' => CustomerResource::make($customer)
+            ], 201);
+        } catch (Exception $e) {
+            dd($e);
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong!',
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Customer retrieved successfully',
+            'result' => CustomerResource::make(Customer::findOrFail($id))
+        ], 200);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,15 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/users', [UserController::class, '']);
 
     Route::post('/logout', [AuthController::class, "logout"]);
+
+    Route::prefix('customers')->group(function () {
+        Route::get('/', [CustomerController::class, 'index']);
+        Route::post('/', [CustomerController::class, 'store']);
+        Route::get('/{id}', [CustomerController::class, 'show']);
+        Route::put('/{id}', [CustomerController::class, 'update']);
+        Route::delete('/{id}', [CustomerController::class, 'destroy']);
+    });
+    
 });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
